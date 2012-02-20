17c8e4c16a821a5df1cd6d50034bcc6d87cd4639
Your Name <martijn.courteaux@skynet.be>
2012-02-21 00:52:07 +0100

    Moved Camera class to the "rendering" package.

--- CraftMania/src/org/craftmania/rendering/Camera.java
+++ CraftMania/src/org/craftmania/rendering/Camera.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package org.craftmania.world;
+package org.craftmania.rendering;
 
 import org.craftmania.datastructures.ViewFrustum;
 import org.craftmania.game.Game;
